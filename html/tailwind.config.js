b1fdb840184611032236773310675ffd36ac9939
Eloïse <eloise-martin@live.fr>
2021-10-11 18:36:21 +0200

    TwicPics demo: html x homepage template

---
+++ html/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['src/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'daily-dev-tips': '#F89283',
+				'twicpics': '#9000ff',
+      },
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+		require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+	]
+};
